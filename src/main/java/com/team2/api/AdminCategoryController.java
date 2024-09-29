51903107ef0d48af0252177376929f1a6b200cd0
Gustavo Cruz Leon <gcruzl3@upao.edu.pe>
2024-09-29 15:23:54 -0500

    feat(backend): implementación del CRUD de categoria

--- src/main/java/com/team2/api/AdminCategoryController.java
+++ src/main/java/com/team2/api/AdminCategoryController.java
@@ -1,7 +1,58 @@
 package com.team2.api;
 
+import com.team2.model.entity.Category;
+import com.team2.service.AdminCategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/admin/categories")
 public class AdminCategoryController {
-    void hello () {
-        System.out.println("hello");
+    private final AdminCategoryService adminCategoryService;
+
+    @GetMapping
+    public ResponseEntity<List<Category>> getAllCategories() {
+        List<Category> categories = adminCategoryService.getAll();
+        return new ResponseEntity<List<Category>>(categories,HttpStatus.OK);
+    }
+
+    @GetMapping("/page")
+    public ResponseEntity<Page<Category>> paginateCategories (
+            @PageableDefault(size =5, sort ="name")Pageable pageable) {
+        Page<Category> categories = adminCategoryService.paginate(pageable);
+        return new ResponseEntity<Page<Category>>(categories,HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Category> getCategoryById(@PathVariable("id") Integer id) {
+        Category category = adminCategoryService.findById(id);
+        return new ResponseEntity<Category>(category,HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Category> createCategory(@RequestBody Category category) {
+        Category newCategory = adminCategoryService.create(category);
+        return new ResponseEntity<Category>(newCategory,HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Category> updateCategory(@PathVariable("id") Integer id,
+                                                   @RequestBody Category category) {
+        Category updatedCategory = adminCategoryService.update(id, category);
+        return new ResponseEntity<Category>(updatedCategory,HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Category> deleteCategory(@PathVariable("id") Integer id) {
+        adminCategoryService.delete(id);
+        return new ResponseEntity<Category>(HttpStatus.NO_CONTENT);
     }
 }
--- src/main/java/com/team2/repository/CategoryRepository.java
+++ src/main/java/com/team2/repository/CategoryRepository.java
@@ -1,5 +1,7 @@
 package com.team2.repository;
 
-public interface CategoryRepository {
-    void hello ();
+import com.team2.model.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Integer> {
 }
